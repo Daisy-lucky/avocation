b50749ac04b89e1823fe324437c65d5edebc2272
kindle <1335150837@qq.com>
2024-03-04 23:32:15 +0800

    添加了评论的数据库，并初步跑通了新建

--- src/main/java/com/my/avocation/controller/MainController.java
+++ src/main/java/com/my/avocation/controller/MainController.java
@@ -1,15 +1,13 @@
 package com.my.avocation.controller;
 
-import com.my.avocation.Service.LabelService;
+import com.my.avocation.Service.CommentService;
 import com.my.avocation.entity.Label;
-import com.my.avocation.entity.User;
 import com.my.avocation.utils.LabelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
@@ -18,22 +16,10 @@ public class MainController {
     private LabelRepository labelRepository;
 
     // Map the "/index" URL to the "user" method
-    @GetMapping("/index")
-    public String user(Model model){
-        // Add a "name" attribute to the model with a value of "test"
-        model.addAttribute("name","test");
-        // Return the "index" view
-        return  "index";
-    }
 
-    @GetMapping("/user")
-    public String users(Model model){
-        User user = new User();
-        user.setAge(10);
-        user.setName("hp");
-        model.addAttribute("name",user);
-        return "user";
-    }
+    @Autowired
+    private CommentService commentService;
+
 
     @GetMapping("/label")
     public String labels(Model model) {
@@ -42,4 +28,9 @@ public class MainController {
         return "label";
     }
 
+    @PostMapping("comment")
+    public String comment(Model model) {
+        commentService.addComment();
+        return "comment";
+    }
 }
